50decd13b5a2505ff8c627a5f7379eda6640ded6
truecapehorn <truecapehorn@gmail.com>
2018-08-09 09:56:04 +0200

    dodanie nower wersji bo nie bylo netu. Modyfikacja wyswietlania rejestrow

---
+++ src/modbus_rtu_v5.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pymodbus.client.sync import ModbusSerialClient as ModbusClient
+from pymodbus.constants import Endian
+from pymodbus.payload import BinaryPayloadDecoder
+from pymodbus.payload import BinaryPayloadBuilder
+from collections import OrderedDict
+import time
+import numpy as np
+
+
+class Master():
+    ''' Obsłoga Modbus RTU'''
+
+    def __init__(self, method='rtu', port='', speed=9600, stopbits=1, parity='N', bytesize=8, timeout=1):
+        self.method = method
+        self.port = port
+        self.speed = speed
+        self.stopbits = stopbits
+        self.parity = parity
+        self.bytesize = bytesize
+        self.timeout = timeout
+        self.client = ModbusClient(method=self.method, port=self.port, baudrate=self.speed, stopbits=self.stopbits,
+                                   parity=self.parity, bytesize=self.bytesize, timeout=self.timeout)
+        self.connection = self.client.connect()
+        print(
+            "Connection: {}\nmethod = {},\nport = {},\nbaudrate = {},\nstopbits = {},\nparity = {},\nbytesize = {},\ntimeout = {},\n".format(
+                self.connection,
+                self.method,
+                self.port,
+                self.speed,
+                self.stopbits,
+                self.parity,
+                self.bytesize,
+                self.timeout, ))
+
+    def read_register(self, unit, reg_start, reg_lenght, reg_type='holding', data_type='int'):
+        """Funkcja glowna odczytu rejestrow, wywolujaca inne podfunkcje"""
+        # TODO: Nie wiem czy nazwa jest prawidlowa. Niby czyta rejestry ale tak naprawde obsluguje odczyt
+        # TODO: Jeszcze bardziej rozdzielic zadania funkcji na mniejsze funkcje
+
+        client = self.client
+        parm = [unit, reg_start, reg_lenght]
+        try:
+            client.connect()  # TO CHYBA POTRZEBNE JEZELI ZAMYCKAM SESJE PRZY KAZDYM KONCU POMIARU
+            time.sleep(0.2)
+            if reg_type == 'holding':
+                data = self.reg_holding(client, parm)
+            elif reg_type == "input":
+                data = self.reg_input(client, parm)
+            client.close()
+            measure = self.choise_data_type(data, data_type)
+            self.display_data(measure,unit,reg_start)
+            return True
+        except AttributeError:
+            print('Połaczenie z adresem {} nie udane'.format(parm[0]))
+            client.close()
+        except KeyboardInterrupt:
+            print('Przerwanie przez urzytkownika')
+            client.close()
+
+    def reg_holding(self, client, parm):
+        massure = client.read_holding_registers(parm[1], parm[2], unit=parm[0])
+        return massure.registers[0:]
+
+    def reg_input(self, client, parm):
+        massure = client.read_input_registers(parm[1], parm[2], unit=parm[0])
+        return massure.registers[0:]
+
+    def choise_data_type(self, data, data_type):
+        """Jezli data bedzie typu long to trzeba zrobic rekompozycje rejestrow 16bit"""
+        if data_type != 'int':
+            data[0::2], data[1::2] = data[1::2], data[0::2]
+            data_arr = np.array([data], dtype=np.int16)
+            data_as_float = data_arr.view(dtype=np.float32)
+            data = data_as_float
+        else:
+            pass
+        return data
+
+    def display_data(self, data,unit,reg_start):
+        dic_val={}
+        for nr,v in enumerate(data):
+            dic_val[nr+reg_start]=v
+        print("Urzadzenie {} - {}".format(unit,dic_val))
+
+
+# TODO: DODAC ZAPIS REJSTROW
+# TODO: DODAC POMNIEJSZE WYSPECIALIZOWANE MODULY DO ZMIANY ADRESU PREDKOSCI I MOZE JAKIS INNNE
+
+if __name__ == '__main__':
+    apar = Master(port='com2', speed=2400)
+    fif = Master(port='com2', speed=9600)
+    connections = [apar, fif]
+    for nr, conn in enumerate(connections):
+        if conn.connection == False:
+            print("Polaczenie: ", nr, "False!!!!!!!!!!")
+
+    while apar.connection:
+        units=[2,17,18,22,23,3]
+        for i in units:
+            apar.read_register(i, 0, 2)
+            time.sleep(0.5)
+        print(160*"=")
+
+        time.sleep(2)
