c68fdbc1077bed78d00623060bbd3b509b6d478e
truecapehorn <truecapehorn@gmail.com>
2018-10-23 11:18:05 +0200

    dzila odczyt hildingow i intow po roznych portach i dla róznych adrsow unit id i narazie wystraczy

--- TCP_Master.py
+++ TCP_Master.py
@@ -13,11 +13,11 @@ parser = argparse.ArgumentParser(
         --------------------------------------------------------
         '''))
 
-parser.add_argument('-a', '--host', action='store', dest='host', help='Adres hosta')
+parser.add_argument('-a', '--host', action='store', dest='host',type=str, help='Adres hosta')
 
-parser.add_argument('-c', '--port', action='store', dest='port', help='Port')
+parser.add_argument('-c', '--port', nargs='*', dest='port', type=int,help='Port')
 
-parser.add_argument('-u', '--unit', nargs='*', dest='units', type=int, help='Tablica adresow urzadzen')
+parser.add_argument('-u', '--units', nargs='*', dest='units', type=int, help='Tablica adresow urzadzen')
 
 parser.add_argument('-s', '--start', action='store', dest='reg_start', type=int, help='Start zapytania')
 
@@ -31,7 +31,7 @@ parser.add_argument('-i', '--int', action='store_const', dest='data_type', const
 
 parser.add_argument('-f', '--float', action='store_const', dest='data_type', const='float', help='Rejetry typu float')
 
-parser.add_argument('-q', '--qty', action='store', dest='qty', default=3, type=int, help='Ilosc powtorzen def: 3')
+parser.add_argument('-q', '--qty', action='store', dest='qty', default=1, type=int, help='Ilosc powtorzen def: 1')
 
 parser.add_argument('--version', action='version', version='%(prog)s 1.0')
 
@@ -41,9 +41,20 @@ if actions['reg_type'] == None or actions['data_type'] == None:
     print('\n!!! Trzeba wprowadzic typ rejetsru ( holding ?) oraz typ danych w rejestrach (int ?) !!!')
     sys.exit(1)
 
-master = Master(actions['host'], actions['port'])
-master.masterDoc()
-for c, v in enumerate(range(0, actions['qty']), 1):
-    print('Pomiar ', c)
-    for u in actions['units']:
-        master.read_register(u, actions['reg_start'], actions['reg_lenght'], actions['reg_type'], actions['data_type'])
+for port in actions['port']:
+    master = Master(actions['host'], port)
+    master.masterDoc()
+    if master.connection==True:
+        for c, v in enumerate(range(0, actions['qty']), 1):
+            print(100*'=')
+            print('Pomiar ', c)
+            for u in actions['units']:
+                master.read_register(u, actions['reg_start'], actions['reg_lenght'], actions['reg_type'], actions['data_type'])
+            print(100 * '=')
+    else:
+        print('\n!!! Połaczenie z adresem {} na porcie {} nie udane !!!'.format(actions['host'],port))
+        continue
+
+
+
+# actions={'host': '192.168.0.35', 'port': [502], 'units': [1], 'reg_start': 0, 'reg_lenght': 10, 'reg_type': 'holding','data_type': 'int', 'qty': 1}
