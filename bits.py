3e4e7fa8c69b44860d21390d0e7dda00bbb04685
truecapehorn <truecapehorn@gmail.com>
2018-10-25 14:53:57 +0200

    reformat code

--- bits.py
+++ bits.py
@@ -1,17 +1,14 @@
 from src.modbus_tcp_v1 import Master
 
-master = Master('192.168.0.35',502)
+master = Master('192.168.0.35', 502)
 
+mw255 = master.read_coils(0, 255, unit=1)
 
-
-mw255=master.read_coils(0, 255, unit=1)
-
-
-mw= master.read_coils(2000, 2000, unit=1)
-dic_val = {'M{}'.format(nr): {'val':v, 'comment':None} for nr, v in enumerate(mw)}
-
+mw = master.read_coils(2000, 2000, unit=1)
+dic_val = {'M{}'.format(nr): {'val': v, 'comment': None} for nr, v in enumerate(mw)}
 
 print(dic_val)
+
 
 # for k,v in dic_val.items():
 #     print('{} - {} '.format(k,v))
@@ -21,30 +18,27 @@ print(dic_val)
 
 def maping():
     F = open('komentarze_fatek_biuro.csv', 'r')
-    lista=[]
+    lista = []
     for line in F.readlines():
         parts = line.split(';')
-        part=[x.strip() for x in parts]
+        part = [x.strip() for x in parts]
         lista.append(part)
     F.close()
-    slownik={}
+    slownik = {}
     for i in lista:
-        slownik.update({i[0]:{'comment':i[1]}})
+        slownik.update({i[0]: {'comment': i[1]}})
     return slownik
-fatek=maping()
+
+
+fatek = maping()
 
 print(fatek)
-
 
 for item, value in dic_val.items():  # {'M0': False, 'M1': False,..}
     for zmienna, wartosc in fatek.items():  # {'X0': {'komentarz': 'jarzeniówki kory',...}
         if zmienna == item:
-            dic_val[item]['comment']=wartosc['comment']
+            dic_val[item]['comment'] = wartosc['comment']
 print(dic_val)
-for k,v in dic_val.items():
-    if v['comment']!=None:
-        print(v['comment'],'\t',v['val'])
-
-
-
-
+for k, v in dic_val.items():
+    if v['comment'] != None:
+        print(v['comment'], '\t', v['val'])
