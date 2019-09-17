6e79b7938c7bb8d41e8dd040e78e6df07af03ac0
truecapehorn <truecapehorn@gmail.com>
2019-09-17 18:19:38 +0200

    slabe to musze sie zdecydowc co chce

--- Modbus_API/modbus_slave.py
+++ Modbus_API/modbus_slave.py
@@ -11,6 +11,7 @@ from pymodbus.datastore import ModbusSequentialDataBlock
 from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
 
 from twisted.internet.task import LoopingCall
+import json
 
 
 import time
@@ -43,18 +44,47 @@ class Slave:
     def __init__(self,id_start,id_range):
         self.id_start = id_start
         self.id_range = id_range
+        self.path = 'register.json'
+
+    def _open_config(self):
+        with open(self.path,'r') as json_file:
+            return json.load(json_file)
+
+    def get_registers(self):
+        return self._open_config()
+
+    def dump_data(self,data):
+        with open(self.path, 'w') as outfile:
+            json.dump(data, outfile)
+
+    def create_data(self):
+        slaves = {}
+        for i in range(self.id_start, self.id_range + 1):
+            slaves[i] = {
+                'di': [False] * 16,
+                'co': [False] * 16,
+                'hr': [1] * 16,
+                'ir': [0] * 16
+            }
+        return slaves
 
     def create_context(self):
         slaves = {}
-        for i in range(self.id_start,self.id_range+1):
-            slaves[i]=ModbusSlaveContext(
-                di=ModbusSequentialDataBlock(0, [0] * 255),
-                co=ModbusSequentialDataBlock(0, [0] * 255),
-                hr=ModbusSequentialDataBlock(0, [0] * 255),
-                ir=ModbusSequentialDataBlock(0, [0] * 255))
+        registers = self.get_registers()
+        for k,v in registers.items():
+            slaves[int(k)]=ModbusSlaveContext(
+                di = ModbusSequentialDataBlock(0, v["di"]),
+                co = ModbusSequentialDataBlock(0, v["co"]),
+                hr = ModbusSequentialDataBlock(0, v["hr"]),
+                ir = ModbusSequentialDataBlock(0, v["ir"]))
+
         self.context = ModbusServerContext(slaves=slaves, single=False)
 
         return self.context
+
+    def get_context_val(self,context,slave_id,register,address,count):
+
+        return context[slave_id].getValues(register, address, count=count)  # pobranie contextu z tla
 
     def updating_writer(self,a):
         self.a =a
@@ -68,19 +98,35 @@ class Slave:
         context = self.a[0] # ModbusSlaveContext
         register = 3  # ? the function we are working with 3 holding 4 input
         slave_id = 0x01  # ? Adres slave dla ktorego chcemy zmienic wartosc
-        address = 0x10  # 17 rejestr
+        address = 0x00  # 17 rejestr
         # todo: odczytanie jsona
-        values = context[slave_id].getValues(register, address, count=5)  # pobranie contextu z tla
+
+        # values = self.get_context_val(context,slave_id,register,address,16) # pobranie contextu z tla
         # todo: jezli valuses_plik != values : to zapis do pliku
         # todo: trzeba podzielic rejestry do zapisu i do odczytu,
         # todo: rejetry wr to beda mialy priorytet zapisu
 
-        values = [v + 1 for v in values]  # zwikszenie rejestru
-        log.debug("new values: " + str(values))
-        context[slave_id].setValues(register, address, values)  # ustawineie rejestru
+        # values = [v + 1 for v in values]  # zwikszenie rejestru
+        # log.debug("new values: " + str(values))
+        # context[slave_id].setValues(register, address, values)  # ustawineie rejestru
 
-
-
+        register = [1,2,3,4]
+        slave_id = [int(k) for k in self.get_registers()]
+        data ={}
+        values = {}
+        mapa = {1: 'di', 2: 'co', 3: 'hr', 4: 'ir'}
+        for sl in slave_id:
+            for reg in register:
+                values[reg] = self.get_context_val(context, sl, reg, address, 16)  # pobranie contextu z tla
+                values[reg] = [v + 1 for v in values]  # zwikszenie rejestru
+                log.debug("new values: " + str(values))
+                context[sl].setValues(reg, address, values)  # ustawineie rejestru
+            di ={}
+            for k,v in values.items():
+                di[mapa[k]]=v
+            data[sl] = di
+        print(data)
+        self.dump_data(data)
 
 
 class TCP_slave(Slave):
@@ -115,7 +161,8 @@ if __name__=='__main__':
     # context=sl.create_contexts()
     # StartTcpServer(context, identity=identity, address=("localhost", 5021))
 
-    sl2 = TCP_slave(1,20,port=5027)
+    sl2 = TCP_slave(1,5,port=5027)
+
     sl2.run_server()
 
 
